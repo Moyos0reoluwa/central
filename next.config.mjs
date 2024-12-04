6c4ecc3f82f54998999b0aa3cac6bad75f3dd562
moyos0reoluwa <oreoluwaoyedele2@gmail.com>
2024-12-04 13:41:00 +0100

    Commit all changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-    reactStrictMode: true, 
+    reactStrictMode: true,
     output: 'export',
-    basePath: isProd ? '/cs' : '',
-    assetPrefix: isProd ? '/cs/' : '',
-};
-
-export default nextConfig;
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
